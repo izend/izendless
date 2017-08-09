8d1e1f8130385a66c0ec587fab04487d1a4ea50b
frasq <frasq@frasq.org>
2017-08-09 18:46:50 +0200

    Adds folder files

---
+++ files/sysinfo.php
@@ -0,0 +1,4 @@
+<p><?php echo php_uname('s'); ?>&nbsp;<?php echo php_uname('r'); ?><br />
+<?php echo $_SERVER['SERVER_SOFTWARE']; ?><br />
+MySQL <?php echo db_version(); ?><br />
+PHP <?php echo phpversion(); ?></p>
